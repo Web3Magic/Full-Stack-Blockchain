64bd059cce01bff17098691da282cbd005cda95c
Bryan Fullen <fullenbryan@gmail.com>
2021-11-10 18:28:18 -0800

    added a testing script for block

---
+++ block.test.js
@@ -0,0 +1,14 @@
+describe('Block', () =>{
+  const timestamp = 'a-date';
+  const lastHash = 'foo-hash';
+  const hash = 'bar-hash';
+  const data = ["lockchain", 'data'];
+  const block = new Block({ timestamp,lastHash,hash,data });
+
+  it('has a timestamp, lastHash, hash, and a data property', () => {
+    expect(block.timestamp).toEqual(timestamp);
+    expect(block.lastHash).toEqual(lastHash);
+    expect(block.hash).toEqual(hash);
+    expect(block.data).toEqual(data);
+  });
+});
