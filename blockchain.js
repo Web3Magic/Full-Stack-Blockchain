34abe4fb5bd31fabdab74568163a691f701855ee
Bryan Fullen <fullenbryan@gmail.com>
2021-11-22 12:38:20 -0800

    Created statement to isolate the two failing tests to actual last hash

--- blockchain.js
+++ blockchain.js
@@ -13,6 +13,24 @@ class Blockchain {
 
    this.chain.push(newBlock);
  }
+
+  static isValidChain (chain) {
+    if (JSON.stringify(chain[0]) !== JSON.stringify(Block.genesis())) {
+      return false
+    }; 
+
+    for (let i=1; i<chain.length; i++) {
+      const block = chain[1];
+
+      const actualLastHash = chain[i-1].hash;
+
+      const { timestamp, lastHash, hash, data } = block;
+      
+      if (lastHash !== actualLastHash) return false;
+    }
+
+    return true;
+  }
 }
 
 module.exports = Blockchain;
