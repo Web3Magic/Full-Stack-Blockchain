c446d6d30b99cc29f97a961be8b7f08f21f2038e
Bryan Fullen <fullenbryan@gmail.com>
2021-12-07 13:26:25 -0800

    added do while loop and restructured for nonce and difficulty

--- block.js
+++ block.js
@@ -2,25 +2,38 @@ const { GENESIS_DATA } = require('./config');
 const cryptoHash = require('./crypto-hash');
 
 class Block {
-   constructor({ timestamp, lastHash, hash, data }) {
+   constructor({ timestamp, lastHash, hash, data, nonce, difficulty }) {
      this.timestamp = timestamp;
      this.lastHash = lastHash;
      this.hash = hash;
      this.data = data;
+     this.nonce = nonce;
+     this.difficulty = difficulty;
   }
 
   static genesis() {
     return new this(GENESIS_DATA);
   }
   static mineBlock({ lastBlock, data }) {
-    const timestamp = Date.now();
+    let hash, timestamp;
+    //const timestamp = Date.now();
     const lastHash = lastBlock.hash;
+    const { difficulty } = lastBlock;
+    let nonce = 0;
+
+    do {
+      nonce++;
+      timestamp = Date.now();
+      hash = cryptoHash(timestamp,lastHash, data, nonce, difficulty);
+    } while (hash.substring(0, difficulty) !== '0'.repeat(difficulty));
 
     return new this({
       timestamp,
       lastHash,
       data,
-      hash: cryptoHash(timestamp, lastHash, data)
+      difficulty,
+      nonce,
+      hash
     });
   }
 }
