b4501e5f73ce69af52e7ddd0a9a05b2b7ea79a12
Bryan Fullen <fullenbryan@gmail.com>
2022-01-07 13:49:13 -0800

    adding limit to ensure mine rate does not reach zero

--- block.test.js
+++ block.test.js
@@ -91,19 +91,11 @@ describe('Block', () =>{
         originalBlock: block, timestamp: block.timestamp + MINE_RATE + 100
       })).toEqual(block.difficulty-1);
     });
-  });
 
-  describe('adjustDifficulty()', () => {
-    it('raises the difficulty for a quickly mined block', () => {
-      expect(Block.adjustDifficulty({ 
-        originalBlock: block, timestamp: block.timestamp + MINE_RATE - 100 
-      })).toEqual(block.difficulty+1);
-    });
-    
-    it('lowers the difficulty for a slowly mined block', () => {
-      expect(Block.adjustDifficulty({
-        originalBlock: block, timestamp: block.timestamp + MINE_RATE + 100
-      })).toEqual(block.difficulty-1);
+    it('has a lower limit of one', () => {
+      block.difficulty= -1;
+
+      expect(Block.adjustDifficulty({ originalBlock: block})).toEqual(1);
     });
   });
 
