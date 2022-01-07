44d596420d341133ac4e24a6b88263b3b7af0472
Bryan Fullen <fullenbryan@gmail.com>
2022-01-07 12:10:49 -0800

    made difficulty dynamic with let, in do loop line 27 added difficulty class that calls adjust difficulty on the block being looped over

--- block.js
+++ block.js
@@ -15,15 +15,16 @@ class Block {
     return new this(GENESIS_DATA);
   }
   static mineBlock({ lastBlock, data }) {
+    const lastHash = lastBlock.hash;
     let hash, timestamp;
     //const timestamp = Date.now();
-    const lastHash = lastBlock.hash;
-    const { difficulty } = lastBlock;
+    let { difficulty } = lastBlock;
     let nonce = 0;
 
     do {
       nonce++;
       timestamp = Date.now();
+      difficulty = Block.adjustDifficulty({ originalBlock: lastBlock, timestamp });
       hash = cryptoHash(timestamp,lastHash, data, nonce, difficulty);
     } while (hash.substring(0, difficulty) !== '0'.repeat(difficulty));
 
