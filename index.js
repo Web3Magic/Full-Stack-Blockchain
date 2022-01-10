4bca4c17da0ea3a453542d265cd496eab465a49e
Bryan Fullen <fullenbryan@gmail.com>
2022-01-10 14:06:36 -0800

    Post Request to the blockchain

--- index.js
+++ index.js
@@ -3,11 +3,22 @@ const Blockchain = require('./blockchain');
 
 //create express app
 const app = express();
+
 const blockchain = new Blockchain();
+
 //get the blockchains block
 app.get('/api/blocks', (req, res) => {
   res.json(blockchain.chain);
 });
 
+//Post to the blockchain
+app.post('/api/mine', (req, res) => {
+ const { data } = req.body;
+ blockchain.addBlock({ data });
+
+ res.redirect('/api/blocks');
+});
+
+//Port Listener on 3000 
 const PORT = 3000;
 app.listen(PORT, () => console.log(`listening on localhost:${PORT}`));
