2e8f588c44bb043ababbad0ac5febc1399749b71
Bryan Fullen <fullenbryan@gmail.com>
2022-01-11 14:00:27 -0800

    created pubsub implimentation, should add async to fire subscribe correctly

---
+++ pubsub.js
@@ -0,0 +1,26 @@
+const redis = require('redis');
+
+const CHANNELS = {
+  TEST: 'TEST'
+};
+
+class PubSub {
+  constructor() {
+    this.publisher = redis.createClient();
+    this.subscriber = redis.createClient();
+
+    this.subscriber.subscribe(CHANNELS.TEST);
+    this.subscriber.on(
+      'message', 
+      (channel, message) => this.handleMessage(channel, message)
+    );
+  }
+
+  handleMessage(channel,message) {
+    console.log(`Message Received. Channel: ${message}.`);
+  }
+}
+
+const testPubSub = new PubSub();
+
+testPubSub.publisher.publish(CHANNELS.TEST, 'foo');
